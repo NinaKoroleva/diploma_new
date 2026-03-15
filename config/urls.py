07b8277487f815dd7073aff2562492066ceb7eb1
Nina <ni_koroleva@mail.ru>
2026-03-15 10:26:32 +0300

    vo4+urls

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,33 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
-from backend.views import (
-    RegisterView,
-    ProductListView,
-    BasketView,
-    OrderConfirmView
-)
+def home(request):
+    return HttpResponse("API is running")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
-
-    path('user/register', RegisterView.as_view()),
-    path('products', ProductListView.as_view()),
-    path('basket', BasketView.as_view()),
-    path('order/confirm', OrderConfirmView.as_view()),
 ]
+
+
+# #from django.contrib import admin
+# #from django.urls import path, include
+#
+# #from backend.views import (
+#     RegisterView,
+#     ProductListView,
+#     BasketView,
+#     OrderConfirmView
+# )
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('backend.urls')),
+#
+#     path('user/register', RegisterView.as_view()),
+#     path('products', ProductListView.as_view()),
+#     path('basket', BasketView.as_view()),
+#     path('order/confirm', OrderConfirmView.as_view()),
+# ]
